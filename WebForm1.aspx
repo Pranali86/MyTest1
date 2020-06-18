3092363993cadb4dec4e0b0f9c6fca334f10134e
Pranali86 <67089138+Pranali86@users.noreply.github.com>
2020-06-18 10:51:29 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="crudmvc.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a href="https://www.tutorialsteacher.com/linq/why-linq" onclick="return confirm('Are sure wants to delete?');">abc</a>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Button"  OnClick="Button1_Click"/>
+        <asp:Button ID="Button2" runat="server" Text="Delegates" OnClick="Button2_Click" />
+    </form>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>
+
+        $(document).ready(function () {
+            debugger
+            Confirm();
+        });
+        function Confirm() {
+            debugger
+            var confirm_value = document.createElement("INPUT");
+            confirm_value.type = "hidden";
+            confirm_value.name = "confirm_value";
+            if (confirm("Do you want to save data?")) {
+                debugger
+                confirm_value.value = "Yes";
+            } else {
+                debugger
+                confirm_value.value = "No";
+            }
+            document.forms[0].appendChild(confirm_value);
+        }
+
+    </script>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using crudmvc.ConstructorStudy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace crudmvc
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public delegate void MyDelegateString(string text);
+        //public delegate string MyDelegateString(string text); //for string
+        public delegate int MyDelegate(int x, int y);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void OnConfirm()
+        {
+            string confirmValue = Request.Form["confirm_value"];
+            if (confirmValue == "Yes")
+            {
+                this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You clicked YES!')", true);
+            }
+            else
+            {
+                this.Page.ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You clicked NO!')", true);
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a = 1990;
+            Classconst cs = new Classconst(a);
+        }
+        public static void ShowText(string text)// in case return type string then void
+        {
+            Console.WriteLine(text);
+            //return text;  // for string
+        }
+        static int Sum(int x, int y) 
+        {
+            return x + y;
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //MyDelegate d = new MyDelegate(Sum);
+            // int result = d.Invoke(12, 15);
+            //Response.Write("<srcipt>alert('" + result + "')</srcipt>");
+            MyDelegateString d = new MyDelegateString(ShowText);
+            //string msg = d("Hello World..."); // for string
+            d("Hello World...");
+           // Response.Write("<srcipt>alert('" + msg + "')</srcipt>");
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+        }
+    }
+}
